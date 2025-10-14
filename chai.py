08dad46475f349d50771e15fa8ffc7a23f5cdc85
yugan12345 <jainyugan13@gmail.com>
2025-10-14 17:47:23 +0530

    changes made

--- chai.py
+++ chai.py
@@ -1,14 +1,14 @@
 import time
 
-def timer(func):
+def debug(func):
     def wrapper(*args,**kwargs):
-        start=time.time()
+        args_value=', '.join(str(arg) for arg in args)
+        kwargs_value=', '.join (f"{k},{v}" for k,v in kwargs.items())
         result=func(*args,**kwargs)
-        end=time.time()
-        print(f"{func.__name__} ran in {end-start} time")
+        print(f"calling {func.__name__} with args-{args_value} and kwargs-{kwargs_value}")
         return result
     return wrapper
-@timer
-def example_func(n):
-    time.sleep(n)
-example_func(2)
+@debug
+def greet(name,greeting="Hanji"):
+    print(f"{name}, {greeting}")
+greet("yo","chai")
