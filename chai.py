1ad809b01e412873bc5f62e31d4d913c682a43c3
yugan12345 <jainyugan13@gmail.com>
2025-10-28 15:41:42 +0530

    added a storage for your youtube videos and a code to manage them add as you please in the data

--- chai.py
+++ chai.py
@@ -1,14 +1,31 @@
-import time
+import numpy as np
+from scipy.stats import lognorm, gumbel_r
+import matplotlib.pyplot as plt
+s_lognorm = 0.5 
+scale_lognorm = np.exp(1)
 
-def debug(func):
-    def wrapper(*args,**kwargs):
-        args_value=', '.join(str(arg) for arg in args)
-        kwargs_value=', '.join (f"{k},{v}" for k,v in kwargs.items())
-        result=func(*args,**kwargs)
-        print(f"calling {func.__name__} with args-{args_value} and kwargs-{kwargs_value}")
-        return result
-    return wrapper
-@debug
-def greet(name,greeting="Hanji"):
-    print(f"{name}, {greeting}")
-greet("yo","chai")
+
+loc_gumbel = 2.0
+scale_gumbel = 1.0
+
+
+x = np.linspace(0, 10, 500)
+
+pdf_lognorm = lognorm.pdf(x, s=s_lognorm, scale=scale_lognorm)
+
+
+pdf_gumbel = gumbel_r.pdf(x, loc=loc_gumbel, scale=scale_gumbel)
+
+plt.figure(figsize=(10, 6))
+plt.plot(x, pdf_lognorm, label=f'Log-normal (s={s_lognorm}, scale={scale_lognorm:.2f})', color='blue')
+plt.plot(x, pdf_gumbel, label=f'Gumbel (loc={loc_gumbel}, scale={scale_gumbel})', color='red', linestyle='--')
+
+plt.title('Comparison of Log-normal and Gumbel Distributions')
+plt.xlabel('Value')
+plt.ylabel('Probability Density')
+plt.legend()
+plt.grid(True)
+plt.ylim(bottom=0)
+plt.xlim(left=0)
+
+plt.show()
--- hello_word.py
+++ hello_word.py
@@ -1,6 +1,48 @@
-print("hello world")
+import json
+def list_all_videos(video):
+    for ind,video in enumerate(video,start=1):
+        print(f"{ind}-{video['name']}")
+def add_video(video):
+    name=input("add the name of the video: ")
+    time=input("enter video time: ")
+    video.append({'name':name,'time':time})
+    save_data_helper(video)
+def update_video(video):
+    pass
+def delete_video(video):
+    pass
+def load_data():
+    try:
+        with open('youtube.txt','r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 
-def chai(n):
-    print(n)
+def save_data_helper(video):
+    with open('youtube.txt','w') as file:
+        json.dump(video,file)
+def main():
+    videos=load_data()
+    while(True):
+        print("YoutubeMangaer")
+        print("1. list all the videos")
+        print("2. Add a youtube video")
+        print("3. update a videos detail")
+        print("4. Delete A video")
+        print("5.  Exit App")
+        choice=input("enter choice here: ")
+        if(choice=='1'):
+            list_all_videos(videos)
+        elif(choice=='2'):
+            add_video(videos)
+        elif(choice=='3'):
+            update_video(videos)
+        elif(choice=='4'):
+            delete_video(videos)
+        elif(choice=='5'):
+            break
+        else:
+            print("INVALID CHOICE")
 
-chai(3)
+if __name__ =="__main__":
+    main()
